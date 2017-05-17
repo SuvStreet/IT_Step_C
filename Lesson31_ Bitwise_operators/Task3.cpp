5cc2e75b6395670fda6f3656ff73384de201ad81
SuvStreet <suvstreet@gmail.com>
2017-05-17 10:40:06 +0300

    Update Task3.cpp

--- Lesson31_ Bitwise_operators/Task3.cpp
+++ Lesson31_ Bitwise_operators/Task3.cpp
@@ -1,24 +1,30 @@
+// Дан массив целых чисел, в котором все числа кроме одного встречаются четное количество раз,
+// одно число встречается нечетное количество раз. Найти это число.
+// Решить задачу используя битовые операции.
+
 #include<iostream>
 #include<cstring>
 using namespace std;
 
 int main()
 {
-	int arr[] = { 1, 1, 2, 2, 3, 4, 4, 5, 5 }; // сортируем массив
+	int arr[] = { 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5 }; // сортируем массив
 	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
 	{
 		cout << arr[i] << " ";
 	}
+
 	cout << endl;
-	for (int i = 1; i < sizeof(arr)/sizeof(int); i+=2) // шаг = 2, ибо по условию
+
+	for (int i = 1; i < sizeof(arr) / sizeof(int); i += 2)
 	{
-		if (arr[i] ^ arr[i - 1]) // с помощью xor сравниваем значения, если они равны, то даст 0
+		if (arr[i] ^ arr[i - 1])
 		{
-			cout << arr[i - 1]; 
+			cout << arr[i - 1];
 			break;
 		}
 	}
-	
+
 	cout << endl;
 	return 0;
 }
